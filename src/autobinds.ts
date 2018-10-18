68f396fdfbeaa2af7ded904651eda63a038d7a42
Alexandr Polyakov <ahushh@gmail.com>
2018-10-18 17:05:40 +0300

    clean code

--- src/autobinds.ts
+++
@@ -1,8 +0,0 @@
-// import { AutobindRoutesService } from './service/autobind-routes';
-// import { Post } from './entity/post';
-
-// // tslint:disable-next-line:variable-name
-// export function autobindRoutes(Container) {
-//   const service = Container.get(AutobindRoutesService);
-//   service.bindRouteToEntity('posts', Post);
-// }
--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -20,34 +20,5 @@ router.post('/boards/:boardSlug/threads', threadController.create);
 
 router.post('/threads/:threadId/posts', postController.create);
 router.post('/attachments', fileUploadMiddleware, attachmentController.create);
-// router.post(
-//   '/boards/:boardSlug/threads',
-//   [bindBoard],
-//   threadController.create,
-// );
-// router.get(
-//   '/boards/:boardSlug/threads',
-//   [bindBoard],
-//   threadController.list,
-// );
-
-// /* threads */
-// const bindThread = autoBindModelMiddleware({
-//   threads: { entity: Thread, name: 'thread', idField: 'id' },
-// });
-// router.get(
-//   '/threads/:threadId/posts',
-//   [bindThread],
-//   threadController.get,
-// );
-
-// router.post(
-//   '/threads/:threadId/posts',
-//   [bindThread],
-//   postController.create,
-// );
-// router.get('/posts', [autoBindModelMiddleware()], postController.index);
-// router.post('/posts', [autoBindModelMiddleware()], postController.create);
-// router.get('/posts/:postId', [autoBindModelMiddleware()], postController.get);
 
 export default router;
