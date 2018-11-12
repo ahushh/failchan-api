2135164807500fb411ecdf7fb756c9eed8bc8446
Alexandr Polyakov <ahushh@gmail.com>
2018-11-12 14:17:48 +0300

    comment subscriber

--- src/app/subscriber/post.subscriber.ts
+++ src/app/subscriber/post.subscriber.ts
@@ -1,19 +1,19 @@
-import {
-  EntitySubscriberInterface, EventSubscriber,
-  InsertEvent, Repository, UpdateEvent,
-} from 'typeorm';
-import { Post } from '../../domain/entity/post';
+// import {
+//   EntitySubscriberInterface, EventSubscriber,
+//   InsertEvent, Repository, UpdateEvent,
+// } from 'typeorm';
+// import { Post } from '../../domain/entity/post';
 
-@EventSubscriber()
-export class PostSubscriber implements EntitySubscriberInterface<Post> {
+// @EventSubscriber()
+// export class PostSubscriber implements EntitySubscriberInterface<Post> {
 
-  listenTo() {
-    return Post;
-  }
+//   listenTo() {
+//     return Post;
+//   }
 
-  async beforeInsert(event: InsertEvent<Post>) {
+//   async beforeInsert(event: InsertEvent<Post>) {
 
-  }
-  async beforeUpdate(event: UpdateEvent<Post>) {
-  }
-}
+//   }
+//   async beforeUpdate(event: UpdateEvent<Post>) {
+//   }
+// }
