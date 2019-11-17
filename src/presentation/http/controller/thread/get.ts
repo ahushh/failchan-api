04c50437543860c1e330a746be973e5ff0a0c0e1
ahushh <ahushh@gmail.com>
2019-11-18 02:02:30 +0300

    fix(*): fix env and type errors to pass all tests

--- src/presentation/http/controller/thread/get.ts
+++ src/presentation/http/controller/thread/get.ts
@@ -4,6 +4,6 @@ import { ThreadService } from '../../../../app/service/thread.service';
 
 export async function threadsGetAction(request: Request, response: Response) {
   const service = Container.get(ThreadService);
-  const thread = await service.getThreadWithPosts(request.params.threadId);
+  const thread = await service.getThreadWithPosts(+request.params.threadId);
   response.json({ thread });
 }
