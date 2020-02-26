a828dd34f91254245c258982e5610302aa67c90c
ahushh <ahushh@gmail.com>
2020-02-26 22:18:13 +0300

    Fix redis config

--- src/infra/utils/create-pubsub-connection.ts
+++ src/infra/utils/create-pubsub-connection.ts
@@ -1,8 +1,14 @@
 import Redis from 'ioredis';
 import { IPubSubConnection } from '../../app/interfaces/IPubSubConnection';
 export const createPubSubConnection = () => {
-  const pub = new Redis(process.env.REDIS_URI);
-  const sub = new Redis(process.env.REDIS_URI);
+  const pub = new Redis({
+    host: process.env.REDIS_HOST as string,
+    port: +(process.env.REDIS_PORT as string),
+  });
+  const sub = new Redis({
+    host: process.env.REDIS_HOST as string,
+    port: +(process.env.REDIS_PORT as string),
+  });
   return Promise.all([
     new Promise((resolve, reject) => pub.on('ready', () => resolve())),
     new Promise((resolve, reject) => sub.on('ready', () => resolve())),
--- src/infra/utils/create-redis-connection.ts
+++ src/infra/utils/create-redis-connection.ts
@@ -4,7 +4,10 @@ export interface IRedisConnection {
 
 }
 export const createRedisConnection = () => {
-  const connection = new Redis(process.env.REDIS_URI);
+  const connection = new Redis({
+    host: process.env.REDIS_HOST as string,
+    port: +(process.env.REDIS_PORT as string),
+  });
   return new Promise<IRedisConnection>((resolve, reject) => {
     connection.on('ready', () => {
       connection.config('SET', 'notify-keyspace-events', 'Ex');
