da07a3c9ec0f54b30cc4cf6df4c70d9b556545a1
ahushh <ahushh@gmail.com>
2020-03-11 20:02:35 +0300

    Add stacktrace to errors

--- src/app/errors/error.interface.ts
+++ src/app/errors/error.interface.ts
@@ -13,42 +13,7 @@ export const APP_ERRORS = {
 export type AppErrorName = keyof typeof APP_ERRORS;
 
 export interface IAppError<D = any> {
-    /**
-     * Error uniq name
-     *t
-     * @type {AppErrorName}
-     * @memberof IAppError
-     */
-
-  name: AppErrorName;
-    /**
-     * User friendly message
-     *
-     * @type {string}
-     * @memberof IAppError
-     */
-  message: string;
-    /**
-     * Original error for debug purpose
-     *
-     * @type {Error}
-     * @memberof IAppError
-     */
-  error?: Error;
-
-    /**
-     * Any additional information
-     *
-     * @type {*}
-     * @memberof IAppError
-     */
-  details?: D;
-
-  json(): { name: IAppError['name']; message: string; details: D | null; };
-}
-
-export abstract class AppErrorAbstract<D = any> {
-    /**
+  /**
    * Error uniq name
    *t
    * @type {AppErrorName}
@@ -56,31 +21,70 @@ export abstract class AppErrorAbstract<D = any> {
    */
 
   name: AppErrorName;
-    /**
-     * User friendly message
-     *
-     * @type {string}
-     * @memberof IAppError
-     */
-  message = '';
-    /**
-     * Original error for debug purpose
-     *
-     * @type {Error}
-     * @memberof IAppError
-     */
+  /**
+   * User friendly message
+   *
+   * @type {string}
+   * @memberof IAppError
+   */
+  message: string;
+  /**
+   * Original error for debug purpose
+   *
+   * @type {Error}
+   * @memberof IAppError
+   */
   error?: Error;
 
-    /**
-     * Any additional information
-     *
-     * @type {*}
-     * @memberof IAppError
-     */
+  /**
+   * Any additional information
+   *
+   * @type {*}
+   * @memberof IAppError
+   */
   details?: D;
 
-  json(): { name: IAppError['name']; message: string; details: D | null; };
+  json(): { name: IAppError['name']; message: string; details: D | null; stack: string | null };
+}
+
+export abstract class AppErrorAbstract<D = any> implements IAppError {
+  /**
+ * Error uniq name
+ *t
+ * @type {AppErrorName}
+ * @memberof AppErrorAbstract
+ */
+
+  name: AppErrorName;
+  /**
+   * User friendly message
+   *
+   * @type {string}
+   * @memberof AppErrorAbstract
+   */
+  message = '';
+  /**
+   * Original error for debug purpose
+   *
+   * @type {Error}
+   * @memberof AppErrorAbstract
+   */
+  error?: Error;
+
+  /**
+   * Any additional information
+   *
+   * @type {*}
+   * @memberof AppErrorAbstract
+   */
+  details?: D;
+
   json() {
-    return { name: this.name, message: this.message, details: this.details || null };
+    return {
+      name: this.name,
+      message: this.message,
+      details: this.details || null,
+      stack: (this.error && this.error.stack) || null,
+    };
   }
 }
--- src/presentation/http/express.ts
+++ src/presentation/http/express.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import express, { Application } from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import { AppErrorUnexpected } from '../../app/errors/unexpected';
 
 export const configAppFactory = ({ port }) => (app: Application) => {
   app.set('port', port);
@@ -15,11 +16,15 @@ export const configAppFactory = ({ port }) => (app: Application) => {
 export const errorConfigAppFactory = () => (app: Application) => {
   app.use((err, req, res, next) => {
     // tslint:disable-next-line: prefer-template
-    console.error('Unhandled error: ' + JSON.stringify(err));
+    console.error('Unhandled error: ' + JSON.stringify(err), err.stack);
+    if (err instanceof AppErrorUnexpected) {
+      return res.status(500).json(err.json());
+    }
     res.status(500).json({
       code: 'UnhandledError',
       message: 'WTF',
       details: JSON.stringify(err),
+      stack: err.stack,
     });
   });
 };
